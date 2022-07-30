be1119b491d8719ae3c1c92a4efe0b708b4692b7
atdt <jasond1016@gmail.com>
2022-07-30 16:25:32 +0800

    Project 1 - 1. Linked List Deque

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,159 @@
+package deque;
+
+import java.util.Iterator;
+
+public class LinkedListDeque<T> {
+    Node<T> sentinel;
+    int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node<>((T) new Object());
+        Node<T> temp = sentinel;
+        sentinel.prev = temp;
+        sentinel.next = temp;
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        Node<T> temp = sentinel.next;
+        Node<T> curr = new Node<>(item, sentinel, temp);
+        sentinel.next.prev = curr;
+        sentinel.next = curr;
+        size++;
+    }
+
+    public void addLast(T item) {
+        Node<T> temp = sentinel.prev;
+        Node<T> curr = new Node<>(item, temp, sentinel);
+        sentinel.prev.next = curr;
+        sentinel.prev = curr;
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+                
+    }
+
+    public void printDeque() {
+        Node<T> p = sentinel;
+        while (p.next != sentinel) {
+            System.out.print(p.next.obj + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (size <= 0) {
+            return null;
+        }
+        Node<T> first = sentinel.next;
+        sentinel.next = first.next;
+        first.next.prev = sentinel;
+        size--;
+        return first.obj;
+    }
+
+    public T removeLast() {
+        if (size <= 0) {
+            return null;
+        }
+        Node<T> last = sentinel.prev;
+        sentinel.prev = last.prev;
+        last.prev.next = sentinel;
+        size--;
+        return last.obj;
+    }
+
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        Node<T> p = sentinel;
+        for (int i = 0; i < index; i++) {
+            p = p.next;
+        }
+        return p.obj;
+    }
+
+    public Iterator<T> iterator() {
+        Node<T> p = sentinel;
+        return new LinkedListDequeIterator<>(p);
+    }
+
+    private static class LinkedListDequeIterator<T> implements Iterator<T> {
+        Node<T> last;
+        Node<T> curr;
+        public LinkedListDequeIterator(Node<T> sentinel) {
+            this.curr = sentinel;
+            this.last = sentinel.prev;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return curr != last;
+        }
+
+        @Override
+        public T next() {
+            curr = curr.next;
+            return curr.obj;
+        }
+    }
+
+    public boolean equals(Object o) {
+        if (!(o instanceof LinkedListDeque<?>)) {
+            return false;
+        }
+        if (this.size != ((LinkedListDeque<?>) o).size) {
+            return false;
+        }
+        Iterator<?> iteratorO = ((LinkedListDeque<?>) o).iterator();
+        Iterator<?> iteratorT = this.iterator();
+        while (iteratorO.hasNext()) {
+            if (!iteratorO.next().equals(iteratorT.next())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public T getRecursive(int index) {
+        Node<T> p = sentinel.next;
+        return getRecursiveHelper(p, index);
+    }
+
+    private T getRecursiveHelper(Node<T> curr, int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        if (index == 0) {
+            return curr.obj;
+        }
+        return getRecursiveHelper(curr.next, index - 1);
+    }
+
+    private static class Node<T> {
+        T obj;
+        Node<T> prev;
+        Node<T> next;
+
+        public Node() {
+        }
+
+        public Node(T obj) {
+            this.obj = obj;
+        }
+
+        public Node(T obj, Node<T> prev, Node<T> next) {
+            this.obj = obj;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+}
--- proj1/deque/LinkedListDequeTest.java
+++ proj1/deque/LinkedListDequeTest.java
@@ -1,6 +1,9 @@
 package deque;
 
 import org.junit.Test;
+
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 
@@ -14,8 +17,6 @@ public class LinkedListDequeTest {
      * && is the "and" operation. */
     public void addIsEmptySizeTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
@@ -34,15 +35,13 @@ public class LinkedListDequeTest {
 
 		System.out.println("Printing out deque: ");
 		lld1.printDeque();
-		*/
+
     }
 
     @Test
     /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
     public void addRemoveTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty
 		assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
@@ -54,15 +53,13 @@ public class LinkedListDequeTest {
 		lld1.removeFirst();
 		// should be empty
 		assertTrue("lld1 should be empty after removal", lld1.isEmpty());
-        */
+
     }
 
     @Test
     /* Tests removing from an empty deque */
     public void removeEmptyTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
         lld1.addFirst(3);
 
@@ -77,14 +74,13 @@ public class LinkedListDequeTest {
         errorMsg += "  actual size() returned 0\n";
 
         assertEquals(errorMsg, 0, size);
-        */
+
     }
 
     @Test
     /* Check if you can create LinkedListDeques with different parameterized types*/
     public void multipleParamTest() {
 
-        /*
         LinkedListDeque<String>  lld1 = new LinkedListDeque<String>();
         LinkedListDeque<Double>  lld2 = new LinkedListDeque<Double>();
         LinkedListDeque<Boolean> lld3 = new LinkedListDeque<Boolean>();
@@ -96,15 +92,13 @@ public class LinkedListDequeTest {
         String s = lld1.removeFirst();
         double d = lld2.removeFirst();
         boolean b = lld3.removeFirst();
-        */
+
     }
 
     @Test
     /* check if null is return when removing from an empty LinkedListDeque. */
     public void emptyNullReturnTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 
         boolean passed1 = false;
@@ -112,15 +106,13 @@ public class LinkedListDequeTest {
         assertEquals("Should return null when removeFirst is called on an empty Deque,", null, lld1.removeFirst());
         assertEquals("Should return null when removeLast is called on an empty Deque,", null, lld1.removeLast());
 
-        */
+
     }
 
     @Test
     /* Add large number of elements to deque; check if order is correct. */
     public void bigLLDequeTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
         for (int i = 0; i < 1000000; i++) {
             lld1.addLast(i);
@@ -134,6 +126,142 @@ public class LinkedListDequeTest {
             assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
         }
 
-        */
+
+    }
+
+    @Test
+    /* Adds a few things to the list, checking iterator() is correct */
+    public void iteratorTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        lld1.addFirst("front");
+        lld1.addLast("middle");
+        lld1.addLast("back");
+        assertEquals(3, lld1.size());
+
+        Iterator<String> iterator = lld1.iterator();
+        assertTrue(iterator.hasNext());
+        assertEquals("front", iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals("middle", iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals("back", iterator.next());
+
+        assertFalse(iterator.hasNext());
+        assertEquals(3, lld1.size());
+    }
+
+    @Test
+    /* Tests iterate from an empty deque */
+    public void emptyIteratorTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        assertEquals(0, lld1.size());
+
+        Iterator<String> iterator = lld1.iterator();
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    /* Test equals true when two list are same */
+    public void equalsTrueTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        lld1.addFirst("front");
+        lld1.addLast("middle");
+        lld1.addLast("back");
+
+        LinkedListDeque<String> lld2 = new LinkedListDeque<String>();
+        lld2.addFirst("front");
+        lld2.addLast("middle");
+        lld2.addLast("back");
+
+        assertTrue(lld1.equals(lld2));
+        assertTrue(lld2.equals(lld1));
+    }
+
+    @Test
+    /* Test equals false when different size */
+    public void equalsDifferentSizeTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        lld1.addFirst("front");
+        lld1.addLast("back");
+
+        LinkedListDeque<String> lld2 = new LinkedListDeque<String>();
+        lld2.addFirst("front");
+        lld2.addLast("middle");
+        lld2.addLast("back");
+
+        assertFalse(lld1.equals(lld2));
+        assertFalse(lld2.equals(lld1));
+    }
+
+    @Test
+    /* Test equals false when same size but different order */
+    public void equalsSameSizeTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        lld1.addFirst("1");
+        lld1.addLast("2");
+
+        LinkedListDeque<String> lld2 = new LinkedListDeque<String>();
+        lld2.addFirst("2");
+        lld2.addLast("1");
+
+        assertFalse(lld1.equals(lld2));
+        assertFalse(lld2.equals(lld1));
+    }
+
+    @Test
+    /* Test equals false when same "value" but different type */
+    public void equalsDifferentTypeTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        lld1.addFirst("1");
+        lld1.addLast("2");
+
+        LinkedListDeque<Integer> lld2 = new LinkedListDeque<>();
+        lld2.addFirst(1);
+        lld2.addLast(2);
+
+        assertFalse(lld1.equals(lld2));
+        assertFalse(lld2.equals(lld1));
+    }
+
+    @Test
+    /* Test equals true when two are both empty */
+    public void equalsEmptyTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        LinkedListDeque<String> lld2 = new LinkedListDeque<String>();
+
+        assertTrue(lld1.equals(lld2));
+        assertTrue(lld2.equals(lld1));
+    }
+
+    @Test
+    /* Tests get recursive from deque */
+    public void getRecursiveTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        lld1.addFirst("front");
+        lld1.addLast("middle");
+        lld1.addLast("back");
+        assertEquals("front", lld1.getRecursive(0));
+        assertEquals("middle", lld1.getRecursive(1));
+        assertEquals("back", lld1.getRecursive(2));
+        assertEquals(null, lld1.getRecursive(-1));
+        assertEquals(null, lld1.getRecursive(3));
+    }
+
+    @Test
+    /* Tests get recursive from an empty deque */
+    public void getRecursiveEmptyTest() {
+
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+        assertEquals(null, lld1.getRecursive(0));
+        assertEquals(null, lld1.getRecursive(1));
+        assertEquals(null, lld1.getRecursive(-1));
     }
 }
